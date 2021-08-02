7817399aa2f896b5e01dd0727d60c63f1491476b
MeinLiX <meinlix@ukr.net>
2021-08-02 15:06:15 +0300

    add a request logger

--- BudgetFrogServer/Startup.cs
+++ BudgetFrogServer/Startup.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Text;
 
 namespace BudgetFrogServer
 {
@@ -12,11 +15,32 @@ namespace BudgetFrogServer
         {
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.Use(async (context, next) =>
+                {
+                    StringBuilder sb = new();
+                    sb.Append($"Http Request Information: {Environment.NewLine}" +
+                              $"Schema:{context.Request.Scheme}{Environment.NewLine}" +
+                              $"Host: {context.Request.Host}{Environment.NewLine}" +
+                              $"Path: {context.Request.Path}{Environment.NewLine}" +
+                              $"Body: {Environment.NewLine}");
+                    try
+                    {
+                        foreach (var item in context?.Request?.Form)
+                            sb.Append($"{item.Key} : {item.Value}{ Environment.NewLine}");
+                    }
+                    catch
+                    {
+                        sb.Append($"Empty... { Environment.NewLine}");
+                    }
+                    logger.LogInformation(sb.ToString());
+
+                    await next();
+                });
             }
 
             app.UseRouting();
