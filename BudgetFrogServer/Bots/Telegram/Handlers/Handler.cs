fb97ed60ec9df139a4745a7b05efa6cbb200edc5
MeinLiX <meinlix@ukr.net>
2021-08-30 14:44:04 +0300

    last progress, remove tgBOT to second project

---
+++ BudgetFrogServer/Bots/Telegram/Handlers/Handler.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace BudgetFrogServer.Bots.Telegram.Handlers
+{
+    public class Handler
+    {
+        public static Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var handler = update.Type switch
+            {
+                // UpdateType.Unknown:
+                // UpdateType.ChannelPost:
+                // UpdateType.EditedChannelPost:
+                // UpdateType.ShippingQuery:
+                // UpdateType.PreCheckoutQuery:
+                // UpdateType.Poll:
+                UpdateType.Message => BotOnMessageReceived(botClient, update.Message),
+                UpdateType.EditedMessage => BotOnMessageReceived(botClient, update.EditedMessage),
+                // UpdateType.CallbackQuery:
+                // UpdateType.InlineQuery:
+                // UpdateType.ChosenInlineResult:
+                _ => UnknownUpdateHandlerAsync(botClient, update)
+            };
+
+            try
+            {
+                await handler;
+            }
+            catch (Exception exception)
+            {
+                await HandleErrorAsync(botClient, exception, cancellationToken);
+            }
+        }
+
+        private static async Task BotOnMessageReceived(ITelegramBotClient botClient, Message message)
+        {
+            Console.WriteLine($"Receive message type: {message.Type}");
+            if (message.Type != MessageType.Text)
+                return;
+
+            await (message.Text.Split(' ').First() switch
+            {
+                "/remove" => RemoveKeyboard(botClient, message),
+                _ => Usage(botClient, message)
+            });
+
+            static async Task<Message> RemoveKeyboard(ITelegramBotClient botClient, Message message)
+            {
+                return await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                                                            text: "Removing keyboard",
+                                                            replyMarkup: new ReplyKeyboardRemove());
+            }
+
+            static async Task<Message> Usage(ITelegramBotClient botClient, Message message)
+            {
+                const string ANSmessage = "work.";
+
+                return await botClient.SendTextMessageAsync(chatId: message.Chat.Id,
+                                                            text: ANSmessage,
+                                                            replyMarkup: new ReplyKeyboardRemove());
+            }
+        }
+
+        private static Task UnknownUpdateHandlerAsync(ITelegramBotClient botClient, Update update)
+        {
+            Console.WriteLine($"Unknown update type: {update.Type}");
+            return Task.CompletedTask;
+        }
+    }
+}
