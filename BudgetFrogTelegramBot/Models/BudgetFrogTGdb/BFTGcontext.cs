e2434f28ca88cbc1d4fb168e9e738a7e5aa7ca2c
MeinLiX <meinlix@ukr.net>
2021-09-08 13:39:46 +0300

    add sqlite  [TGbot]

---
+++ BudgetFrogTelegramBot/Models/BudgetFrogTGdb/BFTGcontext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace BudgetFrogTelegramBot.Models.BudgetFrogTGdb
+{
+    class BFTGcontext : DbContext
+    {
+        //public DbSet<>  { get; set; }
+
+        public BFTGcontext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@$"Filename={Environment.CurrentDirectory + @"\BFTG.db"}");
+        }
+    }
+}
