e6b3fa0ecb91a030d984db2236971fdbe11bda62
MeinLiX <meinlix@ukr.net>
2021-09-08 14:15:44 +0300

    add token access requests

---
+++ BudgetFrogServer/Controllers/TokenAcces/TransactionCategoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using BudgetFrogServer.Models;
+using BudgetFrogServer.Models.Basis;
+using BudgetFrogServer.Utils;
+
+namespace BudgetFrogServer.Controllers.TokenAcces
+{
+    [Route("token/[controller]")]
+    [ApiController]
+    public class TransactionCategoryController : ControllerBase
+    {
+        private readonly DB_Context _base_context;
+
+        public TransactionCategoryController(DB_Context base_context)
+        {
+            _base_context = base_context;
+        }
+
+        [HttpGet("{ExternalToken}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Get(string ExternalToken)
+        {
+            try
+            {
+
+                List<TransactionCategory> foundCategories = _base_context.TransactionCategory
+                                          .Where(category => category.AppIdentityUser.ExternalToken.ToString() == ExternalToken)
+                                          .ToList();
+
+                return new JsonResult(JsonSerialize.Data(
+                        new
+                        {
+                            TransactionCategories = foundCategories
+                        }))
+                {
+                    StatusCode = StatusCodes.Status200OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(JsonSerialize.ErrorMessageText(ex.Message))
+                {
+                    StatusCode = StatusCodes.Status400BadRequest
+                };
+            }
+        }
+    }
+}
---
+++ BudgetFrogServer/Controllers/TokenAcces/TransactionController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BudgetFrogServer.Models;
+using BudgetFrogServer.Models.Basis;
+using BudgetFrogServer.Utils;
+
+namespace BudgetFrogServer.Controllers.TokenAcces
+{
+    [Route("token/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly DB_Context _base_context;
+        private readonly DB_ExchangeRatesContext _ER_context;
+
+        public TransactionController(DB_Context base_context, DB_ExchangeRatesContext ER_context)
+        {
+            _base_context = base_context;
+            _ER_context = ER_context;
+        }
+
+        [HttpGet("{ExternalToken}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Get(string ExternalToken)
+        {
+            try
+            {
+                List<Transaction> foundTransactions = _base_context.Transaction
+                                         .Where(fc => fc.AppIdentityUser.ExternalToken.ToString() == ExternalToken)
+                                         .ToList();
+
+
+                return new JsonResult(JsonSerialize.Data(
+                        new
+                        {
+                            transactions = foundTransactions
+                        }))
+                {
+                    StatusCode = StatusCodes.Status200OK
+                };
+            }
+            catch (Exception ex)
+            {
+                return new JsonResult(JsonSerialize.ErrorMessageText(ex.Message))
+                {
+                    StatusCode = StatusCodes.Status400BadRequest
+                };
+            }
+        }
+    }
+}
