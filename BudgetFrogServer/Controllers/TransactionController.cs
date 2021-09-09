d3f397ccc08a1e3f5a9415847ea522796399f2b0
MeinLiX <meinlix@ukr.net>
2021-09-09 19:07:55 +0300

    fix transaction patch (balance)

--- BudgetFrogServer/Controllers/TransactionController.cs
+++ BudgetFrogServer/Controllers/TransactionController.cs
@@ -235,10 +235,18 @@ namespace BudgetFrogServer.Controllers
 
                 if (RecepitBinary is not null && !RecepitBinary.ContentType.StartsWith("image")) throw new Exception("Invalid file type");
 
-                var transactionFound = await _base_context.Transaction
+                Transaction transactionFound = await _base_context.Transaction
                                                    .Include(t => t.AppIdentityUser)
+                                                   .Include(t => t.TransactionCategory)
                                                    .FirstOrDefaultAsync(transactionQ => transactionQ.ID == transactionBODY.ID && transactionQ.AppIdentityUser.ID == userId);
-
+                Transaction oldTransaction = new()
+                {
+                    Balance = transactionFound.Balance,
+                    Currency = transactionFound.Currency,
+                    AppIdentityUser = transactionFound.AppIdentityUser,
+                    TransactionCategory = transactionFound.TransactionCategory,
+                    TransactionCategoryID = transactionFound.TransactionCategoryID,
+                };
                 _ = transactionFound ?? throw new Exception("Transaction not found.");
 
                 if (transactionFound == transactionBODY)
@@ -255,7 +263,7 @@ namespace BudgetFrogServer.Controllers
                 transactionFound.RecepitBinary = (RecepitBinary is not null) ? new byte[RecepitBinary.Length] : null;
                 transactionFound.RecepitAvailable = RecepitBinary is not null;
 
-                if (RecepitBinary is not null)  
+                if (RecepitBinary is not null)
                     RecepitBinary.OpenReadStream().Read(transactionFound.RecepitBinary);
 
                 _base_context.Transaction.Update(transactionFound);
@@ -266,8 +274,18 @@ namespace BudgetFrogServer.Controllers
                                               .OrderByDescending(er => er.ID)
                                               .FirstOrDefaultAsync();
 
+                #region remove old transaction balance
+                if (oldTransaction.Balance != transactionFound.Balance || oldTransaction.TransactionCategoryID != transactionFound.Balance)
+                {
+                    decimal transactionBalanceOld = (decimal)exchangeRates.Convert(oldTransaction.Currency, oldTransaction.AppIdentityUser.Currency, (float)oldTransaction.Balance);
+                    transactionFound.AppIdentityUser.Balance -= (oldTransaction.TransactionCategory.Income ?? true) ? transactionBalanceOld : -transactionBalanceOld;
+                }
+                #endregion
+
                 decimal transactionBalance = (decimal)exchangeRates.Convert(transactionFound.Currency, transactionFound.AppIdentityUser.Currency, (float)transactionFound.Balance);
                 transactionFound.AppIdentityUser.Balance += (transactionFound.TransactionCategory.Income ?? true) ? transactionBalance : -transactionBalance;
+
+
                 #endregion
                 await _base_context.SaveChangesAsync();
                 #endregion
