543e0c61a20fdf80816715a342cb6f8640d28cc3
MeinLiX <meinlix@ukr.net>
2021-09-09 22:16:44 +0300

    add db controller

---
+++ BudgetFrogTelegramBot/Utils/DB/BudgetFrogTG/MainController.cs
@@ -0,0 +1,10 @@
+﻿using BudgetFrogTelegramBot.Models.BudgetFrogTGdb;
+
+namespace BudgetFrogTelegramBot.Utils.DB.BudgetFrogTG
+{
+    class MainController
+    {
+        private readonly static BFTGcontext _BFTGcontext = new();
+        internal static UserController userController { get; } = new(_BFTGcontext);
+    }
+}
---
+++ BudgetFrogTelegramBot/Utils/DB/BudgetFrogTG/UserController.cs
@@ -0,0 +1,14 @@
+﻿using BudgetFrogTelegramBot.Models.BudgetFrogTGdb;
+
+namespace BudgetFrogTelegramBot.Utils.DB.BudgetFrogTG
+{
+    class UserController
+    {
+        private readonly BFTGcontext _BFTGcontext;
+
+        public UserController(BFTGcontext _BFTGcontext)
+        {
+            this._BFTGcontext = _BFTGcontext;
+        }
+    }
+}
