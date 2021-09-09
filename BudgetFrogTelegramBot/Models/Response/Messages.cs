6921c4c25d5c473a142ab48c1c5d815528bf27c6
MeinLiX <meinlix@ukr.net>
2021-09-09 23:03:02 +0300

    add response models

---
+++ BudgetFrogTelegramBot/Models/Response/Messages.cs
@@ -0,0 +1,25 @@
+﻿
+namespace BudgetFrogTelegramBot.Models.Response
+{
+    class Messages
+    {
+        public class CorrectMessage
+        {
+            public string message { get; set; }
+            public bool success { get; set; }
+            public string time { get; set; }
+        }
+
+        public class IncorrectMessage
+        {
+            public Errors errors { get; set; }
+            public bool success { get; set; }
+            public string time { get; set; }
+        }
+
+        public class Errors
+        {
+            public string message { get; set; }
+        }
+    }
+}
