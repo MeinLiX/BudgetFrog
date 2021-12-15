6b5ea0f5c86b0b6212d3e2bc0cd91125590a58b5
MeinLiX <meinlix@ukr.net>
2021-12-15 23:14:39 +0200

    [WebApi] add BudgetApp context (#1)

---
+++ WepApi/Context/BudgetAppContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WepApi.Context.Interfaces;
+using WepApi.Models.Auth;
+using WepApi.Models.Budgets;
+
+namespace WepApi.Context;
+
+public class BudgetAppContext : DbContext, IBudgetAppContext
+{
+    public BudgetAppContext(DbContextOptions<BudgetAppContext> options)
+        : base(options)
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+    public DbSet<AppIdentityUser> AppIdentityUsers { get; set; }
+    public DbSet<Balance> Balances { get; set; }
+    public DbSet<Budget> Budgets { get; set; }
+    public DbSet<TransactionDescription> TransactionsDescription { get; set; }
+    public DbSet<TransactionDescriptionCategory> TransactionDescriptionCategories { get; set; }
+
+    public async Task<int> SaveChangesAsync() => await base.SaveChangesAsync();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.BuildAppIdentityUser();
+        modelBuilder.BuildBalance();
+        modelBuilder.BuildBudget();
+        modelBuilder.BuildTransactionDescription();
+        modelBuilder.BuildTransactionDescriptionCategory();
+    }
+}
---
+++ WepApi/Context/Interfaces/IBudgetAppContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WepApi.Models.Auth;
+using WepApi.Models.Budgets;
+
+namespace WepApi.Context.Interfaces;
+
+public interface IBudgetAppContext
+{
+    DbSet<AppIdentityUser> AppIdentityUsers { get; set; }
+    DbSet<Balance> Balances { get; set; }
+    DbSet<Budget> Budgets { get; set; }
+    DbSet<TransactionDescription> TransactionsDescription { get; set; }
+    DbSet<TransactionDescriptionCategory> TransactionDescriptionCategories { get; set; }
+
+    Task<int> SaveChangesAsync();
+}
