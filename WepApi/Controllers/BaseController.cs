1678a23493c98b2cadd2d42cbb6c851dbad91e6b
MeinLiX <meinlix@ukr.net>
2021-12-15 23:16:34 +0200

    [WebApi] add BudgetAppContext to services and BaseController (#1)

---
+++ WepApi/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WepApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+}
--- WepApi/Program.cs
+++ WepApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WepApi.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BudgetAppContext>(options =>
+                options.UseNpgsql(builder.Configuration.GetConnectionString("BudgetAppIdentityDB")));
 
 var app = builder.Build();
 
@@ -13,9 +19,6 @@ app.UseHttpsRedirection();
 app.UseRouting();
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllers();
-});
+app.MapControllers();
 
 app.Run();
