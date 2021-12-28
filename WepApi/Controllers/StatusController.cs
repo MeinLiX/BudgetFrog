11dec4c2f49c45cf991f816ea91a48c741a9fa8d
MeinLiX <meinlix@ukr.net>
2021-12-28 12:10:53 +0200

    add auth status and ping endpoints (#8)

--- WepApi/Controllers/StatusController.cs
+++ WepApi/Controllers/StatusController.cs
@@ -1,46 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WepApi.Context;
-using WepApi.Features.UserFutures.Queries;
-using WepApi.Models.Auth;
-using WepApi.Utils.Exceptions;
 using WepApi.Utils.Wrapper;
 
 namespace WepApi.Controllers;
 
 public class StatusController : BaseController
 {
-    private readonly BudgetAppContext _budgetAppContext;
-    private IMediator _mediator;
-    protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
-    public StatusController(BudgetAppContext budgetAppContext)
-    {
-        _budgetAppContext = budgetAppContext;
-    }
 
-    #region Temp, for testing
+    /// <summary>
+    /// Check server activity.
+    /// </summary>
     [HttpGet("ping")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<IActionResult> Ping()
+    public IActionResult Ping() => Ok(Result.Success("Pong!"));
+
+    /// <summary>
+    /// Check user authenticated.
+    /// </summary>      
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public IActionResult IsAuth() => User.Identity.IsAuthenticated switch
     {
-
-        throw new AppException("Test");
-        /*_budgetAppContext.AppIdentityUsers.Add(new AppIdentityUser { FirstName = "yurii", LastName = "hrohul", Password = "HASH", Email = "a@a.a" });
-        await _budgetAppContext.SaveChangesAsync();
-        AppIdentityUser? user = _budgetAppContext.AppIdentityUsers.FirstOrDefault(); //db trigger for test
-
-        return new JsonResult(user)
-        {
-            StatusCode = StatusCodes.Status200OK
-        };
-        */
-    }
-
-    [HttpGet("ping/{id}")]
-    public async Task<IActionResult> GetById(Guid id)
-    {
-        return Ok(
-            Result<AppIdentityUser>.Success(
-                await Mediator.Send(new GetAppIdentityUserByIdQuery { ID = id })));
-    }
-    #endregion Temp, for testing
+        true => Ok(Result.Success("User is Auth!")),
+        false => Unauthorized(Result.Fail("User is not Auth!"))
+    };
 }
