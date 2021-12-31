2ea387d74677a5abf92eb72e117a2ce3b34aa728
MeinLiX <meinlix@ukr.net>
2021-12-31 02:08:40 +0200

    Initial Svelte project (#2)

---
+++ ClientSvelte/src/App.svelte
@@ -0,0 +1,14 @@
+<video src="404.mp4" autoplay muted loop>
+	<track kind="captions" />
+</video>
+
+<style>
+	video {
+		min-width: 200px;
+		width: 50%;
+		height: auto;
+		margin-left: auto;
+		margin-right: auto;
+		display: block;
+	}
+</style>
