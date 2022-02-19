df254a80c5edd091136ad4856496199326ace1b8
MeinLiX <meinlix@ukr.net>
2022-02-19 17:57:55 +0200

    add to budget model name and inviteToken

--- WepApi/Models/Budgets/Budget.cs
+++ WepApi/Models/Budgets/Budget.cs
@@ -5,6 +5,10 @@ namespace WepApi.Models.Budgets;
 
 public class Budget : ModelBase
 {
+    public string Name { get; set; }
+
+    public string InviteToken { get; set; }
+
     public Balance Balance { get; set; }
 
     public List<AppIdentityUser> Users { get; set; } = new();
--- WepApi/Models/Budgets/BudgetBuilder.cs
+++ WepApi/Models/Budgets/BudgetBuilder.cs
@@ -7,5 +7,13 @@ public static class BudgetBuilder
     public static void BuildBudget(this ModelBuilder MB)
     {
         MB.BuildModelBase<Budget>();
+
+        MB.Entity<Budget>()
+          .Property(pb => pb.Name)
+          .HasColumnType("VARCHAR(32)");
+
+        MB.Entity<Budget>()
+         .Property(m => m.InviteToken)
+         .HasColumnType("VARCHAR(32)");
     }
 }
