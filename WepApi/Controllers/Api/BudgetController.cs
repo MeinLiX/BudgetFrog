4c69a5c87313588caa37048b3627a90b4f12f845
MeinLiX <meinlix@ukr.net>
2022-02-21 11:47:51 +0200

    add 'get' and 'create' budget endpoints (20)

---
+++ WepApi/Controllers/Api/BudgetController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WepApi.Features.BudgetFutures.Commands;
+using WepApi.Features.BudgetFutures.Queries;
+
+namespace WepApi.Controllers.Api;
+
+public class BudgetController : BaseController
+{
+    private readonly IMediator _mediator;
+
+    public BudgetController(IMediator Mediator)
+    {
+        _mediator = Mediator;
+    }
+
+    [HttpGet()]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Get()
+    {
+        return Ok(await _mediator.Send(new GetBudgetsByUserIdQuery()));
+    }
+    
+    [HttpPost("create")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create([FromBody] CreateBudgetCommand command)
+    {
+        return Ok(await _mediator.Send(command));
+    }
+    
+    [HttpPatch("update/{BudgetID}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Update(string BudgetID/*todo*/)
+    {
+        return BadRequest();
+    }
+    
+    [HttpDelete("delete/{BudgetID}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Delete(string BudgetID/*todo*/)
+    {
+        return BadRequest();
+    }
+    
+    [HttpPost("invite/{BudgetID}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Invite(string BudgetID/*todo*/)
+    {
+        return BadRequest();
+    }
+    
+    [HttpPost("accept/{InviteToken}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Accept(string InviteToken/*todo*/)
+    {
+        return BadRequest();
+    } 
+    
+    [HttpPost("leave/{BudgetID}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Leave(string BudgetID/*todo*/)
+    {
+        return BadRequest();
+    }
+}
---
+++ WepApi/Features/BudgetFutures/Validators/CreateBudgetCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using WepApi.Features.BudgetFutures.Commands;
+
+namespace WepApi.Features.BudgetFutures.Validators;
+
+public class CreateBudgetCommandValidator : AbstractValidator<CreateBudgetCommand>
+{
+    public CreateBudgetCommandValidator()
+    {
+        RuleFor(r => r.Name)
+            .NotEmpty()
+            .WithMessage("Budget name is required.")
+            .MaximumLength(32)
+            .WithMessage("Budget name must be less than 32 characters.");
+
+        RuleFor(r => r.Currency)
+            .NotEmpty()
+            .WithMessage("Currency is required.");
+    }
+}
