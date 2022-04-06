d7e2de5fe6e05b9da7df9acf31b11b5577156f46
MeinLiX <meinlix@ukr.net>
2022-04-06 15:21:36 +0300

    Add 'generate invite token Budget command' (#20)

---
+++ WepApi/Features/BudgetFutures/Commands/GenerateInviteTokenBudgetCommand.cs
@@ -0,0 +1,49 @@
+﻿using WepApi.Context.Interfaces;
+using WepApi.Features.Services;
+using WepApi.Utils.Wrapper;
+using WepApi.Models.Budgets;
+
+namespace WepApi.Features.BudgetFutures.Commands;
+
+public class GenerateInviteTokenBudgetCommand : IRequest<Utils.Wrapper.IResult>
+{
+    public string BudgetID { get; set; }
+    private Guid GetBudgetID { get => Guid.Parse(BudgetID); }
+
+    public class GenerateInviteTokenBudgetCommandHandler : IRequestHandler<GenerateInviteTokenBudgetCommand, Utils.Wrapper.IResult>
+    {
+        private readonly IBudgetAppContext _context;
+        private readonly SignInManagerService _signInManager;
+        public GenerateInviteTokenBudgetCommandHandler(IBudgetAppContext context, SignInManagerService signInManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+        }
+        public async Task<Utils.Wrapper.IResult> Handle(GenerateInviteTokenBudgetCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _signInManager.GetUser();
+            Budget? budget = _context.Budgets
+                            .Where(b => b.Users.Contains(user))
+                            .FirstOrDefault(b => b.ID == request.GetBudgetID);
+
+            if (budget is null)
+            {
+                return Result.Success($"Budget not found.");
+            }
+            else
+            {
+                //if(budget.InviteToken != "")
+                //{
+                //    return Result.Success($"Invite token is already avaliable.");
+                //}
+
+                budget.InviteToken = Guid.NewGuid().ToString("N");
+                
+                await _context.SaveChangesAsync();
+
+                return Result.Success($"New invite token is now avaliable.");
+            }
+        }
+    }
+
+}
