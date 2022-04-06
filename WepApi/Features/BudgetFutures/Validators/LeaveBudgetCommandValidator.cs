01a55fc382f1d13a0fc759910a2866fa4ada4b15
MeinLiX <meinlix@ukr.net>
2022-04-06 15:20:58 +0300

    Add 'leave Budget command validator' (#20)

---
+++ WepApi/Features/BudgetFutures/Validators/LeaveBudgetCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using WepApi.Features.BudgetFutures.Commands;
+
+namespace WepApi.Features.BudgetFutures.Validators;
+
+public class LeaveBudgetCommandValidator : AbstractValidator<LeaveBudgetCommand>
+{
+    public LeaveBudgetCommandValidator()
+    {
+        RuleFor(c => c.BudgetID)
+            .NotEmpty()
+            .WithMessage("Budget id is required.")
+            .Must(bID => Guid.TryParse(bID, out _))
+            .WithMessage("Incorrect Budget id.");
+    }
+}
