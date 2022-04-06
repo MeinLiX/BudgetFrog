8a9aae0b4965678b93854d546b78c610b8bb3aa5
MeinLiX <meinlix@ukr.net>
2022-04-06 15:27:26 +0300

    Add endpoints with commands to Budget controller (#20)

--- WepApi/Controllers/Api/BudgetController.cs
+++ WepApi/Controllers/Api/BudgetController.cs
@@ -13,59 +13,91 @@ public class BudgetController : BaseController
         _mediator = Mediator;
     }
 
-    [HttpGet()]
+    /// <summary>
+    ///  Get list of user's own Budgets with details.
+    /// </summary>
+    [HttpGet("")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Get()
     {
-        return Ok(await _mediator.Send(new GetBudgetsByUserIdQuery()));
+        return Ok(await _mediator.Send(new GetUserBudgetsQuery()));
     }
-    
-    [HttpPost("create")]
+
+    /// <summary>
+    /// Get user's own Budget with details.
+    /// </summary>
+    [HttpGet("{budgetID}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Get(string budgetID)
+    {
+        return Ok(await _mediator.Send(new GetUserBudgetByBudgetIdQuery() { BudgetID = budgetID }));
+    }
+
+    /// <summary>
+    /// Create Budget.
+    /// </summary>
+    [HttpPost("")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Create([FromBody] CreateBudgetCommand command)
     {
         return Ok(await _mediator.Send(command));
     }
-    
-    [HttpPatch("update/{BudgetID}")]
+
+    /// <summary>
+    /// Ubdate Budget;
+    /// </summary>
+    [HttpPatch("")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Update(string BudgetID/*todo*/)
+    public async Task<IActionResult> Update([FromBody] UpdateBudgetCommand command)
     {
-        return BadRequest();
+        return Ok(await _mediator.Send(command));
     }
-    
-    [HttpDelete("delete/{BudgetID}")]
+
+    /// <summary>
+    /// Generate new token for invite another users into own Budget.
+    /// </summary>
+    [HttpPatch("token")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Delete(string BudgetID/*todo*/)
+    public async Task<IActionResult> GenerateInviteToken([FromBody] GenerateInviteTokenBudgetCommand command)
     {
-        return BadRequest();
+        return Ok(await _mediator.Send(command));
     }
-    
-    [HttpPost("invite/{BudgetID}")]
+
+    /// <summary>
+    /// Remove token for disable invite other users into own Budget.
+    /// </summary>
+    [HttpDelete("token")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Invite(string BudgetID/*todo*/)
+    public async Task<IActionResult> DeactivateInviteToken([FromBody] DeactivateInviteTokenBudgetCommand command)
     {
-        return BadRequest();
+        return Ok(await _mediator.Send(command));
     }
-    
-    [HttpPost("accept/{InviteToken}")]
+
+    /// <summary>
+    /// Join to Budget.
+    /// </summary>
+    [HttpPatch("join/{InviteToken}")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Accept(string InviteToken/*todo*/)
+    public async Task<IActionResult> Join(string InviteToken)
     {
-        return BadRequest();
-    } 
-    
-    [HttpPost("leave/{BudgetID}")]
+        return Ok(await _mediator.Send(new JoinBudgetCommand() { InviteToken = InviteToken }));
+    }
+
+    /// <summary>
+    /// Leave from Budget (If the user is last, Budget will be deleted)
+    /// </summary>
+    [HttpDelete("leave")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Leave(string BudgetID/*todo*/)
+    public async Task<IActionResult> Leave([FromBody] LeaveBudgetCommand command)
     {
-        return BadRequest();
+        return Ok(await _mediator.Send(command));
     }
 }
