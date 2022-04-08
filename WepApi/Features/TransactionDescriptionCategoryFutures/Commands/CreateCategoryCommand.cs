979683e309a9ec6166be3c3cd46126659a726476
MeinLiX <meinlix@ukr.net>
2022-04-09 01:01:04 +0300

    Add 'create category command' (#21)

---
+++ WepApi/Features/TransactionDescriptionCategoryFutures/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,53 @@
+﻿using WepApi.Context.Interfaces;
+using WepApi.Features.Services;
+using WepApi.Models.Transactions;
+using WepApi.Utils.Wrapper;
+
+namespace WepApi.Features.TransactionDescriptionCategoryFutures.Commands;
+
+public class CreateCategoryCommand : IRequest<Utils.Wrapper.IResult>
+{
+    public string Name { get; set; }
+    public bool Income { get; set; } = false;
+    public string Color { get; set; }
+    public string BudgetID { get; set; } = "";
+    private Guid GetBudgetID { get => Guid.Parse(BudgetID); }
+
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, Utils.Wrapper.IResult>
+    {
+        private readonly IBudgetAppContext _context;
+        private readonly SignInManagerService _signInManager;
+        public CreateCategoryCommandHandler(IBudgetAppContext context, SignInManagerService signInManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+        }
+        public async Task<Utils.Wrapper.IResult> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _signInManager.GetUser();
+
+            //check unique name.
+            if (_context.TransactionDescriptionCategories.Any(c =>
+                                                                  c.Budget.ID == request.GetBudgetID &&
+                                                                  c.Budget.Users.Contains(user) &&
+                                                                  c.Name == request.Name))
+            {
+                return Result.Success($"Category name '{request.Name}' already taken.");
+            }
+            else
+            {
+                _context.TransactionDescriptionCategories.Add(new TransactionDescriptionCategory()
+                {
+                    Name = request.Name,
+                    Income = request.Income,
+                    Color = request.Color,
+                    Budget = _context.Budgets.First(b => b.ID == request.GetBudgetID)
+                });
+
+                await _context.SaveChangesAsync();
+
+                return Result.Success($"Category '{request.Name}' has created.");
+            }
+        }
+    }
+}
