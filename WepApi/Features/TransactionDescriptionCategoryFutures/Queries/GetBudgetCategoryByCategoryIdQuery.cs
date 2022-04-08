1276441e2ebe532af72ef02a1968d1d1185d3198
MeinLiX <meinlix@ukr.net>
2022-04-09 00:57:55 +0300

    Add 'get category query' (#21)

---
+++ WepApi/Features/TransactionDescriptionCategoryFutures/Queries/GetBudgetCategoryByCategoryIdQuery.cs
@@ -0,0 +1,42 @@
+﻿using WepApi.Context.Interfaces;
+using WepApi.Features.Services;
+using WepApi.Models.Transactions;
+using WepApi.Utils.Wrapper;
+
+namespace WepApi.Features.TransactionDescriptionCategoryFutures.Queries;
+
+public class GetBudgetCategoryByCategoryIdQuery : IRequest<Result<TransactionDescriptionCategory?>>
+{
+    public string BudgetID { get; set; } = "";
+    public string CategoryID { get; set; }
+    private Guid GetBudgetID { get => Guid.Parse(BudgetID); }
+    private Guid GetCategoryID { get => Guid.Parse(CategoryID); }
+
+    public class GetBudgetCategoryByCategoryIdQueryHandler : IRequestHandler<GetBudgetCategoryByCategoryIdQuery, Result<TransactionDescriptionCategory?>>
+    {
+        private readonly IBudgetAppContext _context;
+        private readonly SignInManagerService _signInManager;
+        public GetBudgetCategoryByCategoryIdQueryHandler(IBudgetAppContext context, SignInManagerService signInManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+        }
+        public async Task<Result<TransactionDescriptionCategory?>> Handle(GetBudgetCategoryByCategoryIdQuery query, CancellationToken cancellationToken)
+        {
+            var user = await _signInManager.GetUser();
+            TransactionDescriptionCategory? category =
+                _context.TransactionDescriptionCategories
+                        .FirstOrDefault(c =>
+                                            c.Budget.ID == query.GetBudgetID &&
+                                            c.Budget.Users.Contains(user) &&
+                                            c.ID == query.GetCategoryID);
+
+            if (category is null)
+            {
+                return Result<TransactionDescriptionCategory?>.Success(category, $"Category not found.");
+            }
+
+            return Result<TransactionDescriptionCategory?>.Success(category);
+        }
+    }
+}
