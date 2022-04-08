7c9ef9cd4fb84a53ce890af0db3a05e9d0cec458
MeinLiX <meinlix@ukr.net>
2022-04-09 01:01:37 +0300

    Add 'delete category command validator' (#21)

---
+++ WepApi/Features/TransactionDescriptionCategoryFutures/Validators/DeleteCategoryCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using WepApi.Features.TransactionDescriptionCategoryFutures.Commands;
+
+namespace WepApi.Features.TransactionDescriptionCategoryFutures.Validators;
+
+public class DeleteCategoryCommandValidator : AbstractValidator<DeleteCategoryCommand>
+{
+    public DeleteCategoryCommandValidator()
+    {
+        RuleFor(c => c.BudgetID)
+            .NotEmpty()
+            .WithMessage("Budget id is required.")
+            .Must(bID => Guid.TryParse(bID, out _))
+            .WithMessage("Incorrect Budget id.");
+
+        RuleFor(c => c.CategoryID)
+            .NotEmpty()
+            .WithMessage("Category id is required.")
+            .Must(bID => Guid.TryParse(bID, out _))
+            .WithMessage("Incorrect Budget id.");
+    }
+}
