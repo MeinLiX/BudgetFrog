937bf27bfd2ffc1cbe31a9c8c044d9377f2b5326
MeinLiX <meinlix@ukr.net>
2022-04-09 00:58:09 +0300

    Add 'get category query validator' (#21)

---
+++ WepApi/Features/TransactionDescriptionCategoryFutures/Validators/GetBudgetCategoryByCategoryIdQueryValidator.cs
@@ -0,0 +1,21 @@
+﻿using WepApi.Features.TransactionDescriptionCategoryFutures.Queries;
+
+namespace WepApi.Features.TransactionDescriptionCategoryFutures.Validators;
+
+public class GetBudgetCategoryByCategoryIdQueryValidator : AbstractValidator<GetBudgetCategoryByCategoryIdQuery>
+{
+    public GetBudgetCategoryByCategoryIdQueryValidator()
+    {
+        RuleFor(c => c.BudgetID)
+            .NotEmpty()
+            .WithMessage("Budget id is required.")
+            .Must(bID => Guid.TryParse(bID, out _))
+            .WithMessage("Incorrect Budget id.");
+
+        RuleFor(c => c.CategoryID)
+            .NotEmpty()
+            .WithMessage("Category id is required.")
+            .Must(bID => Guid.TryParse(bID, out _))
+            .WithMessage("Incorrect category id.");
+    }
+}
