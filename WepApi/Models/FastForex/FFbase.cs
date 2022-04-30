39e924bdef1f5dd4ec1afe3279c9d1a59f595093
MeinLiX <meinlix@ukr.net>
2022-05-01 01:55:07 +0300

    Add FastForex models #22

---
+++ WepApi/Models/FastForex/FFbase.cs
@@ -0,0 +1,29 @@
+﻿using WepApi.Models.Common;
+
+namespace WepApi.Models.FastForex;
+
+public class FFbase : ModelBase
+{
+    public string @base { get; set; }
+    public FFresult results { get; set; }
+    public string updated { get; set; }
+    public int ms { get; set; }
+
+    public decimal GetRate(string from, string to)
+    {
+        if (from == to)
+            return 1;
+
+        if (from == @base && to != @base)
+            return results[to];
+
+        if (from != @base && to == @base)
+            return 1 / results[from];
+
+        return results[to] / results[from];
+    }
+
+    public decimal Exchange(decimal amount, decimal rate) => amount * rate;
+
+    public decimal Convert(string from, string to, decimal amount) => Exchange(amount, GetRate(from, to));
+}
