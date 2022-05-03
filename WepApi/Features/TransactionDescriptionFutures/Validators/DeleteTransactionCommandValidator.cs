869370a73d5f5f2b7ecd8dffd5d64c0a9c4610ce
MeinLiX <meinlix@ukr.net>
2022-05-03 23:33:55 +0300

    Add 'delete transaction command validator' (#23)

---
+++ WepApi/Features/TransactionDescriptionFutures/Validators/DeleteTransactionCommandValidator.cs
@@ -0,0 +1,21 @@
+﻿using WepApi.Features.TransactionDescriptionFutures.Commands;
+
+namespace WepApi.Features.TransactionDescriptionFutures.Validators;
+
+public class DeleteTransactionCommandValidator : AbstractValidator<DeleteTransactionCommand>
+{
+    public DeleteTransactionCommandValidator()
+    {
+        RuleFor(c => c.BudgetID)
+            .NotEmpty()
+            .WithMessage("Budget id is required.")
+            .Must(bID => Guid.TryParse(bID, out _))
+            .WithMessage("Incorrect Budget id.");
+
+        RuleFor(c => c.TransactionID)
+            .NotEmpty()
+            .WithMessage("Transaction id is required.")
+            .Must(bID => Guid.TryParse(bID, out _))
+            .WithMessage("Incorrect Transaction id.");
+    }
+}
