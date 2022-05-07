c51573830a086a1e5db541b6f360af377a8c2c44
MeinLiX <meinlix@ukr.net>
2022-05-08 00:29:20 +0300

    Add 'update planned budget amount command validator' (#24)

---
+++ WepApi/Features/PlannedBudgetFutures/Validators/UpdatePlannedBudgetAmountCommandValidator.cs
@@ -0,0 +1,27 @@
+﻿using WepApi.Features.PlannedBudgetFutures.Commands;
+
+namespace WepApi.Features.PlannedBudgetFutures.Validators;
+
+public class UpdatePlannedBudgetAmountCommandValidator : AbstractValidator<UpdatePlannedBudgetAmountCommand>
+{
+    public UpdatePlannedBudgetAmountCommandValidator()
+    {
+        RuleFor(pb => pb.BudgetID)
+            .NotEmpty()
+            .WithMessage("Budget id is required.")
+            .Must(pb_bID => Guid.TryParse(pb_bID, out _))
+            .WithMessage("Incorrect Budget id.");
+
+        RuleFor(pb => pb.PlannedBudgetID)
+            .NotEmpty()
+            .WithMessage("Planned Budget id is required.")
+            .Must(pb_pID => Guid.TryParse(pb_pID, out _))
+            .WithMessage("Incorrect Planned Budget id.");
+
+        RuleFor(pb => pb.PlannedAmount)
+           .NotEmpty()
+           .WithMessage("Planned amount id is required.")
+           .GreaterThanOrEqualTo(0)
+           .WithMessage("Planned amount cannot be negative.");
+    }
+}
