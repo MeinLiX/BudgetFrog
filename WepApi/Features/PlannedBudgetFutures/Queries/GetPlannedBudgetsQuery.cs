eb515ca14cd0dbd34f8d2c3b4c4e8e0648bfcb6f
MeinLiX <meinlix@ukr.net>
2022-05-08 00:23:49 +0300

    Add 'get planned budgets query' (#24)

---
+++ WepApi/Features/PlannedBudgetFutures/Queries/GetPlannedBudgetsQuery.cs
@@ -0,0 +1,39 @@
+﻿using WepApi.Context.Interfaces;
+using WepApi.Features.Services;
+using WepApi.Models.Budgets;
+using WepApi.Utils.Exceptions;
+using WepApi.Utils.Wrapper;
+
+namespace WepApi.Features.PlannedBudgetFutures.Queries;
+
+public class GetPlannedBudgetsQuery : IRequest<Result<List<PlannedBudget>>>
+{
+    public string BudgetID { get; set; }
+    private Guid GetBudgetID { get => Guid.Parse(BudgetID); }
+
+    public class GetPlannedBudgetsQueryHandler : IRequestHandler<GetPlannedBudgetsQuery, Result<List<PlannedBudget>>>
+    {
+        private readonly IBudgetAppContext _context;
+        private readonly SignInManagerService _signInManager;
+        public GetPlannedBudgetsQueryHandler(IBudgetAppContext context, SignInManagerService signInManager)
+        {
+            _context = context;
+            _signInManager = signInManager;
+        }
+        public async Task<Result<List<PlannedBudget>>> Handle(GetPlannedBudgetsQuery query, CancellationToken cancellationToken)
+        {
+            var user = await _signInManager.GetUser();
+
+            if (!(_context.Budgets.Any(b => b.ID == query.GetBudgetID && b.Users.Contains(user))))
+                throw new AppException("Budget not found");
+
+            return Result<List<PlannedBudget>>.Success(
+                _context.PlannedBudgets.Where(pb => pb.Budget.ID == query.GetBudgetID)
+                                       .Include(pb => pb.PlannedBalance)
+                                       .Include(pb => pb.RealizeBalance)
+                                       .Include(pb => pb.TransactionDescriptionCategory)
+                                       .ToList()
+                );
+        }
+    }
+}
