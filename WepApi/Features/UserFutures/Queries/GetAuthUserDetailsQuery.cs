9c905969962ed64a3597112eee9300efbf32a5aa
MeinLiX <meinlix@ukr.net>
2022-05-12 21:02:05 +0300

    add result wrapper (https://github.com/MeinLiX/BudgetFrog/commit/61dc746a2bbf0facbb904b5480d582c7e89b3016)

--- WepApi/Features/UserFutures/Queries/GetAuthUserDetailsQuery.cs
+++ WepApi/Features/UserFutures/Queries/GetAuthUserDetailsQuery.cs
@@ -1,19 +1,20 @@
 ﻿using WepApi.Features.Services;
 using WepApi.Models.Auth;
+using WepApi.Utils.Wrapper;
 
 namespace WepApi.Features.UserFutures.Queries;
 
-public class GetAuthUserDetailsQuery : IRequest<AppIdentityUser>
+public class GetAuthUserDetailsQuery : IRequest<Result<AppIdentityUser>>
 {
     public Guid ID { get; set; }
-    public class GetAuthUserDetailsQueryHandler : IRequestHandler<GetAuthUserDetailsQuery, AppIdentityUser>
+    public class GetAuthUserDetailsQueryHandler : IRequestHandler<GetAuthUserDetailsQuery, Result<AppIdentityUser>>
     {
         private readonly SignInManagerService _signInManager;
         public GetAuthUserDetailsQueryHandler(SignInManagerService signInManager)
         {
             _signInManager = signInManager;
         }
-        public async Task<AppIdentityUser> Handle(GetAuthUserDetailsQuery query, CancellationToken cancellationToken)
-            => await _signInManager.GetUser();
+        public async Task<Result<AppIdentityUser>> Handle(GetAuthUserDetailsQuery query, CancellationToken cancellationToken)
+            => Result<AppIdentityUser>.Success(await _signInManager.GetUser());
     }
 }
