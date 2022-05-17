56e690aa925113b5f68f76b2f444892b79644611
MeinLiX <meinlix@ukr.net>
2022-05-18 01:10:55 +0300

    update budget(second) header

--- ClientSvelte/src/components/budget/BudgetHeader.svelte
+++ ClientSvelte/src/components/budget/BudgetHeader.svelte
@@ -1,7 +1,7 @@
 <script>
     import {onMount} from "svelte";
-    import {push, location} from "svelte-spa-router";
-
+    import {location} from "svelte-spa-router";
+    import {ErrorWrapper} from "../../services/RequestWrapper"
     import Request from "../../services/RequestController";
     import {selectedBudget} from "../../stores";
 
@@ -9,32 +9,30 @@
         location.subscribe(async (newLocation) => {
             if (newLocation.includes("budget")) {
                 newLocation += "/";
-                let BudgetID = newLocation.substring(
-                    newLocation.indexOf("/", 1) + 1,
-                    newLocation.indexOf("/", 8)
-                );
+                let BudgetID = newLocation.substring(newLocation.indexOf("/", 1) + 1, newLocation.indexOf("/", 8));
                 try {
                     $selectedBudget = (await Request.budget.get({BudgetID})).data;
-                } catch (e) {
+                } catch (err) {
+                    ErrorWrapper(err);
                 }
             }
         });
     });
-    let isOpen = false;
-
-    function handleUpdate(event) {
-        isOpen = event.detail.isOpen;
-    }
 </script>
 
 <div class="navbar bg-yellow-100">
     <div class="navbar-start">
-        <a class="btn btn-ghost normal-case" href="/#/budget/{$selectedBudget.id}">
-            Budget: <b>{$selectedBudget.name}</b>
-            <div class="badge badge-outline">
-                {$selectedBudget.balance?.amount} : {$selectedBudget.balance?.currency}
-            </div>
+        <a class="btn btn-ghost normal-case text-xl" href="/#/budget/{$selectedBudget.id}">
+            <b>{$selectedBudget.name}</b>
         </a>
+    </div>
+    <div class="navbar-center">
+        <div class="text-xl indicator p-1 mt-3">
+            <span class="indicator-item badge badge-outline">
+                 {$selectedBudget.balance?.currency}
+            </span>
+            <b>{$selectedBudget.balance?.amount}</b>
+        </div>
     </div>
     <div class="navbar-end">
         <a class="btn btn-ghost normal-case" href="#/budget/{$selectedBudget.id}/planned">
