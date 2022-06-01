12a334bb98214ef17d832fc4e2093cb92c08ce08
MeinLiX <meinlix@ukr.net>
2022-06-01 15:39:14 +0300

    add center position for charts (statisctic page)

--- ClientSvelte/src/views/Statistic.svelte
+++ ClientSvelte/src/views/Statistic.svelte
@@ -17,53 +17,52 @@
         });
     }
     const UpdateCharts = async () => {
-            loading = true;
-            let transactionsYearSorted = transactions.filter(transaction => new Date(transaction.date).toString().includes(selectedYear));
-            updateUsedCurrencies(transactionsYearSorted);
-            new chartjs(chartTransactionsCanvas.getContext('2d'), {
-                type: 'line',
-                data: {
-                    labels: months,
-                    datasets:
-                        categories.map(category => {
-                            return {
-                                label: category.name,
-                                backgroundColor: rgbaFromHEX(category.color, 0.2),
-                                borderColor: category.color,
-                                data: months.map(month =>
-                                    transactionsYearSorted.filter(transaction =>
-                                        transaction.transactionDescriptionCategory.id === category.id &&
-                                        new Date(transaction.date).toString().includes(month)
-                                    ).length
-                                )
-                            }
-                        })
-                }
-            });
-            new chartjs(radarCurrenciesCanvas.getContext('2d'), {
-                type: 'radar',
-                data: {
-                    labels: categories.map(category => category.name),
-                    datasets: usedCurrencies.map(currency => {
-                        let CurrencyColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+        let transactionsYearSorted = transactions.filter(transaction => new Date(transaction.date).toString().includes(selectedYear));
+        updateUsedCurrencies(transactionsYearSorted);
+        new chartjs(chartTransactionsCanvas.getContext('2d'), {
+            type: 'line',
+            data: {
+                labels: months,
+                datasets:
+                    categories.map(category => {
                         return {
-                            label: currency,
-                            backgroundColor: rgbaFromHEX(CurrencyColor, 0.2),
-                            borderColor: CurrencyColor,
-                            data: categories.map(category =>
+                            label: category.name,
+                            backgroundColor: rgbaFromHEX(category.color, 0.2),
+                            borderColor: category.color,
+                            data: months.map(month =>
                                 transactionsYearSorted.filter(transaction =>
                                     transaction.transactionDescriptionCategory.id === category.id &&
-                                    transaction.balance.currency === currency
+                                    new Date(transaction.date).toString().includes(month)
                                 ).length
                             )
                         }
                     })
-                }
-            });
-            loading = false;
+            }
+        });
+        new chartjs(radarCurrenciesCanvas.getContext('2d'), {
+            type: 'radar',
+            data: {
+                labels: categories.map(category => category.name),
+                datasets: usedCurrencies.map(currency => {
+                    let CurrencyColor = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0');
+                    return {
+                        label: currency,
+                        backgroundColor: rgbaFromHEX(CurrencyColor, 0.2),
+                        borderColor: CurrencyColor,
+                        data: categories.map(category =>
+                            transactionsYearSorted.filter(transaction =>
+                                transaction.transactionDescriptionCategory.id === category.id &&
+                                transaction.balance.currency === currency
+                            ).length
+                        )
+                    }
+                })
+            }
+        });
     }
 
     onMount(async () => {
+        loading = true;
         try {
             transactions = (await Request.transaction.getList({BudgetID: params.budgetID})).data;
             categories = (await Request.category.getList({BudgetID: params.budgetID})).data;
@@ -71,6 +70,7 @@
             ErrorWrapper(err);
         }
         await UpdateCharts();
+        loading = false;
     });
 
     //{budgetID}
@@ -85,17 +85,17 @@
     let transactions = [];
 </script>
 
-<div class="center_content form-control p-4">
-    <input class="input input-bordered text-center" type="number" bind:value={selectedYear} on:blur={UpdateCharts}>
-</div>
-{#if loading}
-    <div class="text-center">
-        <span class="text-xl">Loading...</span>
+<div class="center_content">
+    <div class="form-control p-4">
+        <input class="input input-bordered text-center" type="number" bind:value={selectedYear} on:blur={UpdateCharts}>
     </div>
-{/if}
-<div class="px-20 pt-4 pb-8">
-    <canvas bind:this={chartTransactionsCanvas} id="TransactionsChart"></canvas>
-</div>
-<div class="px-20 pt-4 pb-8">
-    <canvas bind:this={radarCurrenciesCanvas} id="radarCurrenciesCanvas"></canvas>
+    {#if loading}
+        <div class="text-center">
+            <span class="text-xl">Loading...</span>
+        </div>
+    {/if}
+
+    <canvas bind:this={radarCurrenciesCanvas} id="radarCurrenciesCanvas" class="pt-4 pb-8 max-w-7xl"></canvas>
+
+    <canvas bind:this={chartTransactionsCanvas} id="TransactionsChart" class="pt-4 pb-8 max-w-7xl"></canvas>
 </div>
